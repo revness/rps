e85b78e2821582c4a675849c05a5e5b55a9a4b70
Chapman Tang <chapmantang@Chapmans-MacBook-Pro.local>
2023-09-21 21:08:32 +1000

    fixed bug with ties and else condition

--- script.js
+++ script.js
@@ -4,46 +4,36 @@ function getComputerChoice() {
   return choices[randomNumber];
 }
 
-function getPlayerChoice() {
-  const choices = ["rock", "paper", "scissors"];
-  const randomNumber = Math.floor(Math.random() * 3);
-  return choices[randomNumber];
-}
-
 let playerScore = 0;
 let computerScore = 0;
 let ties = 0;
 function playRound(playerSelection, computerSelection) {
   if (playerSelection === computerSelection) {
     ties++;
-
     gameStatus.textContent = `Tie! You both chose ${playerSelection}, player score: ${playerScore}, computer score: ${computerScore}, ties: ${ties}`;
-  }
-  if (playerSelection === "rock") {
+  } else if (playerSelection === "rock") {
     if (computerSelection === "paper") {
       computerScore++;
-      gameStatus.textContent = `Tie! You both chose ${playerSelection}, player score: ${playerScore}, computer score: ${computerScore}, ties: ${ties}`;
+      gameStatus.textContent = `You lose! ${computerSelection} beats ${playerSelection}, player score: ${playerScore}, computer score: ${computerScore}, ties: ${ties}`;
     } else {
       playerScore++;
-      gameStatus.textContent = `Tie! You both chose ${playerSelection}, player score: ${playerScore}, computer score: ${computerScore}, ties: ${ties}`;
+      gameStatus.textContent = `You win! ${playerSelection} beats ${computerSelection}, player score: ${playerScore}, computer score: ${computerScore}, ties: ${ties}`;
     }
-  }
-  if (playerSelection === "paper") {
+  } else if (playerSelection === "paper") {
     if (computerSelection === "scissors") {
       computerScore++;
-      gameStatus.textContent = `Tie! You both chose ${playerSelection}, player score: ${playerScore}, computer score: ${computerScore}, ties: ${ties}`;
+      gameStatus.textContent = `You lose! ${computerSelection} beats ${playerSelection}, player score: ${playerScore}, computer score: ${computerScore}, ties: ${ties}`;
     } else {
       playerScore++;
-      gameStatus.textContent = `Tie! You both chose ${playerSelection}, player score: ${playerScore}, computer score: ${computerScore}, ties: ${ties}`;
+      gameStatus.textContent = `You win! ${playerSelection} beats ${computerSelection}, player score: ${playerScore}, computer score: ${computerScore}, ties: ${ties}`;
     }
-  }
-  if (playerSelection === "scissors") {
+  } else if (playerSelection === "scissors") {
     if (computerSelection === "rock") {
       computerScore++;
-      gameStatus.textContent = `Tie! You both chose ${playerSelection}, player score: ${playerScore}, computer score: ${computerScore}, ties: ${ties}`;
+      gameStatus.textContent = `You lose! ${computerSelection} beats ${playerSelection}, player score: ${playerScore}, computer score: ${computerScore}, ties: ${ties}`;
     } else {
       playerScore++;
-      gameStatus.textContent = `Tie! You both chose ${playerSelection}, player score: ${playerScore}, computer score: ${computerScore}, ties: ${ties}`;
+      gameStatus.textContent = `You win! ${playerSelection} beats ${computerSelection}, player score: ${playerScore}, computer score: ${computerScore}, ties: ${ties}`;
     }
   }
 }
